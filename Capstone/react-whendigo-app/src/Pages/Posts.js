0d90dafe31f5035491fd659fae046c4cf36312be
Harlea R Johnston <98293078+HarleaJohnston@users.noreply.github.com>
2023-08-09 09:16:29 -0600

    Working on getting like and dislike to work
    I feel like I am getting closer

--- Capstone/react-whendigo-app/src/Pages/Posts.js
+++ Capstone/react-whendigo-app/src/Pages/Posts.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
 import Nav from "./Nav";
 
-const Posts = ({ userId }) => {
+const Posts = () => {
   const [items, setPosts] = useState([]);
+  const userId = sessionStorage.getItem('sessionKey');
 
   useEffect(() => {
     fetch("http://localhost:3666/post")
@@ -30,10 +31,16 @@ const Posts = ({ userId }) => {
       alert("Please log in to like or dislike posts.");
       return;
     }
-
-    const newStatus = likeStatus === "liked" ? null : "liked";
-
-    fetch(`http://localhost:3666/post/${postId}/${likeStatus}`, {
+  
+    let newStatus;
+  
+    if (likeStatus === "liked") {
+      newStatus = "unlike"; 
+    } else {
+      newStatus = "like"; 
+    }
+  
+    fetch(`http://localhost:3666/post/${postId}/${newStatus}`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -54,9 +61,9 @@ const Posts = ({ userId }) => {
       alert("Please log in to bookmark posts.");
       return;
     }
-  
+
     const newBookmarkedStatus = !items.find((item) => item.id === postId).bookmarked;
-  
+
     fetch(`http://localhost:3666/post/${postId}/bookmark`, {
       method: "POST",
       headers: {
@@ -64,31 +71,29 @@ const Posts = ({ userId }) => {
       },
       body: JSON.stringify({ userId, bookmarked: newBookmarkedStatus }),
     })
-      .then((response) => response.json())
-      .then((data) => {
-        setPosts((prevPosts) =>
-          prevPosts.map((post) =>
-            post.id === postId ? { ...post, bookmarked: data.bookmarked } : post
-          )
-        );
-      })
-      .catch((error) => {
-        console.error(error);
-      });
+    .then(response => response.json())
+    .then(data => {
+      setPosts(prevPosts => prevPosts.map(post => post.id === postId ? { ...post, bookmarked: data.bookmarked } : post));
+    })
+    .catch(error => {
+      console.error(error);
+    });
   };
 
-    return (
+  return (
+    <div>
       <div>
-        <div>
-          <Nav />
-        </div>
-  
-        <div className="ContentBox">
-          <h2 className="right">Post Feed:</h2>
-          <div className="spacer"></div>
-          <div className="PostBox">
-            {items.map((item) => (
-              <div key={item.id}>
+        <Nav />
+      </div>
+
+      <div className="ContentBox">
+        <h2 className="right">Post Feed:</h2>
+        <div className="spacer"></div>
+        {items.map((item) => {
+          const likeStatus = item.likes.includes(userId) ? "liked" : item.dislikes.includes(userId) ? "disliked" : null;
+
+            return (
+              <div key={item.id} className="PostBox">
                 <h3>{item.postDate}</h3>
                 <p>{item.postImg}</p>
                 <p>{item.postBody}</p>
@@ -97,35 +102,37 @@ const Posts = ({ userId }) => {
                 {userId && (
                   <>
                     <button
-                      onClick={() => handleLikeDislike(item.id, item.likeStatus)}
+                      onClick={() => handleLikeDislike(item.id, likeStatus)}
                       disabled={
-                        item.likeStatus === "disliked" || item.likes.includes(userId)
+                        likeStatus === "disliked" || likeStatus === "liked"
                       }
                     >
-                      {item.likeStatus === "liked" ? "Unlike" : "Like"}
+                      {likeStatus === "liked" ? "Unlike" : "Like"}
                     </button>
                     <button
-                      onClick={() => handleLikeDislike(item.id, item.likeStatus)}
+                      onClick={() => handleLikeDislike(item.id, likeStatus)}
                       disabled={
-                        item.likeStatus === "liked" || item.dislikes.includes(userId)
+                        likeStatus === "liked" || likeStatus === "disliked"
                       }
                     >
-                      {item.likeStatus === "disliked" ? "Undislike" : "Dislike"}
+                      {likeStatus === "disliked" ? "Undislike" : "Dislike"}
                     </button>
                   </>
                 )}
-                {userId && (
-                  <button onClick={() => handleBookmark(item.id)} disabled={item.bookmarked}>
-                    {item.bookmarked ? "Bookmarked" : "Bookmark"}
-                  </button>
-                )}
+              {userId && (
+                <button
+                  onClick={() => handleBookmark(item.id)}
+                  disabled={item.bookmarked}
+                >
+                  {item.bookmarked ? "Bookmarked" : "Bookmark"}
+                </button>
+              )}
               </div>
-            ))}
-          </div>
-          <div className="spacer"></div>
+            );
+          })}
         </div>
       </div>
-    );
-  };
+  );
+};
 
 export default Posts;
