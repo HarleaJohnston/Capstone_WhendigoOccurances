50445286d07620e636b84bf0303ca608967fe608
Harlea R Johnston <98293078+HarleaJohnston@users.noreply.github.com>
2023-08-21 13:04:18 -0600

    Tis a bit dumb
    I'm sleep deprived give me a break lmaoo

--- Capstone/backend/webDal.js
+++ Capstone/backend/webDal.js
@@ -1,8 +1,8 @@
 const { mongoose, Schema } = require("mongoose");
 const bcrypt = require("bcrypt");
 const { v4: uuidv4 } = require("uuid");
-const connectionStringz = "mongodb://127.0.0.1:27017/Whendigo_Occurances_DB";
-//const connectionStringz = "mongodb+srv://quillianrenae:8YDtlJxCKoZrlfgo@cluster0.sv3zblq.mongodb.net/";
+//const connectionStringz = "mongodb://127.0.0.1:27017/Whendigo_Occurances_DB";
+const connectionStringz = "mongodb+srv://quillianrenae:8YDtlJxCKoZrlfgo@cluster0.sv3zblq.mongodb.net/Whendigo_Occurances_DB";
 const collectionOne = "Posts"
 const collectionTwo = "Users"
 
